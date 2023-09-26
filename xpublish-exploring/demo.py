9e7735e7dbd9a9d0a1dcc38c9999f7fb886227f1
Alex Kerney <abk@mac.com>
2023-09-26 12:57:55 -0400

    Xpublish demo
    A demo for building Xpublish plugins and some things that you can do with them

---
+++ xpublish-exploring/demo.py
@@ -0,0 +1,34 @@
+import cf_xarray
+import xarray as xr
+from requests import HTTPError
+
+from xpublish import Plugin, Rest, hookimpl
+
+
+class TutorialDataset(Plugin):
+    name: str = "xarray-tutorial-datasets"
+
+    @hookimpl
+    def get_dataset(self):
+        return list(xr.tutorial.file_formats)
+    
+    @hookimpl
+    def get_dataset(self, dataset_id: str):
+        try:
+            return xr.tutorial.open_dataset(dataset_id)
+        except HTTPError:
+            return None
+
+
+
+
+
+rest = Rest({})
+rest.register_plugin(TutorialDataset())
+
+# from mean import MeanPlugin
+# from lme import LmeSubsetPlugin
+# rest.register_plugin(MeanPlugin())
+# rest.register_plugin(LmeSubsetPlugin())
+
+rest.serve()
